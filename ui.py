9faebbc3ced23de164c67c19c9e2fb430699feda
chy <19281123@bjtu.edu.cn>
2021-08-21 21:25:16 +0800

    Changes

--- ui.py
+++ ui.py
@@ -21,38 +21,42 @@ def start_camera():
     cv2.destroyAllWindows()
 
 def choose_pic():
+
     path = filedialog.askopenfilename()
+    print(path)
 
-    # 生成人脸识别类，并初始化
-    my_face_rec = face_rec()
+    if path is not '':
+        # 生成人脸识别类，并初始化
+        my_face_rec = face_rec()
 
-    while True:
-        pic = cv2.imread(path)
-        my_face_rec.recognize(pic)
-        cv2.imshow('Picture', pic)
-        if cv2.waitKey(20) & 0xFF == ord('q'):
-            break
+        while True:
+            pic = cv2.imread(path)
+            my_face_rec.recognize(pic)
+            cv2.imshow('Picture', pic)
+            if cv2.waitKey(20) & 0xFF == ord('q'):
+                break
 
-    cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
 
 def choose_video():
     path = filedialog.askopenfilename()
 
-    # 生成人脸识别类，并初始化
-    my_face_rec = face_rec()
+    if path is not '':
+        # 生成人脸识别类，并初始化
+        my_face_rec = face_rec()
 
-    # 打开摄像头
-    video = cv2.VideoCapture(path)
+        # 打开摄像头
+        video = cv2.VideoCapture(path)
 
-    while True:
-        temp, draw = video.read()
-        my_face_rec.recognize(draw)
-        cv2.imshow('Video', draw)
-        if cv2.waitKey(20) & 0xFF == ord('q'):
-            break
+        while True:
+            temp, draw = video.read()
+            my_face_rec.recognize(draw)
+            cv2.imshow('Video', draw)
+            if cv2.waitKey(20) & 0xFF == ord('q'):
+                break
 
-    video.release()
-    cv2.destroyAllWindows()
+        video.release()
+        cv2.destroyAllWindows()
 
 def creat_window():
     window = tk.Tk()
@@ -61,9 +65,9 @@ def creat_window():
 
     btn1 = tk.Button(window, text='开启摄像头', command=lambda:[window.destroy(), start_camera()])
     btn1.place(relx=0.1, rely=0.4, relwidth=0.2, relheight=0.2)
-    btn2 = tk.Button(window, text='选择图片', command=lambda:[window.destroy(),choose_pic])
+    btn2 = tk.Button(window, text='选择图片', command=lambda:[window.destroy(), choose_pic()])
     btn2.place(relx=0.4, rely=0.4, relwidth=0.2, relheight=0.2)
-    btn3 = tk.Button(window, text='选择视频', command=lambda:[window.destroy(),choose_video])
+    btn3 = tk.Button(window, text='选择视频', command=lambda:[window.destroy(),choose_video()])
     btn3.place(relx=0.7, rely=0.4, relwidth=0.2, relheight=0.2)
 
     window.mainloop()
